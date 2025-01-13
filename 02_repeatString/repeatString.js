7c8932cf4d25c161b382bfc6e378768fc411e598
jbeda01 <70598361+jbeda01@users.noreply.github.com>
2025-01-13 16:39:45 +0000

    Exercise 02 completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(str, n) {
+  let newstr = '';
+  if(n < 0) return 'ERROR';
+  if(!str.trim().length) return '';
+  for(let i=0; i<n; i++){
+    newstr += str;
+  }
+  return newstr;
 };
 
 // Do not edit below this line
